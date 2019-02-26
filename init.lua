4bdb5a9e29b8da3a8f8b666e3cf63f115671f185
Gerold55 <marleybeare56@gmail.com>
2019-02-26 15:03:54 -0500

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,1 +1,5 @@
 nc_furniture = {}
+
+dofile(minetest.get_modpath('nc_furniture')..'/sit.lua')
+dofile(minetest.get_modpath('nc_furniture')..'/craft.lua')
+dofile(minetest.get_modpath('nc_furniture')..'/nodes.lua')
