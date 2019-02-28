af44cb3fd9bac9d0700b254804cca6947fe97faa
Gerold55 <marleybeare56@gmail.com>
2019-02-27 19:55:42 -0500

    Add files via upload

--- craft.lua
+++ craft.lua
@@ -1,24 +1,24 @@
 nodecore.register_craft({
 		label = "assemble wood chair",
 		nodes = {
-			{match = modname .. ":plank", replace = "air"},
-			{x = -1, z = -1, match = modname .. ":wood", replace = "air"},
-			{x = -1, z = 1, match = modname .. ":furn_legs", replace = "air"},
+			{match = "nc_furniture:plank", replace = "air"},
+			{x = -1, z = -1, match = "nc_furniture:wood", replace = "air"},
+			{x = -1, z = 1, match = "nc_furniture:furn_legs", replace = "air"},
 		},
 		items = {
-			modname .. ":chair_wood"
+			"nc_furniture:chair_wood"
 		}
 	})
 	
 nodecore.register_craft({
 		label = "assemble wood table",
 		nodes = {
-			{match = modname .. ":plank", replace = "air"},
-			{x = -1, z = -1, match = modname .. ":wood", replace = "air"},
-			{x = -1, z = 1, match = modname .. ":stick", replace = "air"},
+			{match = "nc_furniture:plank", replace = "air"},
+			{x = -1, z = -1, match = "nc_furniture:wood", replace = "air"},
+			{x = -1, z = 1, match = "nc_furniture:stick", replace = "air"},
 		},
 		items = {
-			modname .. ":chair_wood"
+			"nc_furniture:chair_wood"
 		}
 	})
 	
@@ -26,13 +26,13 @@ nodecore.register_craft({
 nodecore.register_craft({
 		label = "assemble wood chair",
 		nodes = {
-			{match = modname .. ":plank", replace = "air"},
-			{x = -1, z = -1, match = modname .. ":stick", replace = "air"},
-			{x = 1, z = -1, match = modname .. ":stick", replace = "air"},
-			{x = 2, z = -1, match = modname .. ":stick", replace = "air"},
-			{x = 3, z = -1, match = modname .. ":stick", replace = "air"},
+			{match = "nc_furniture:plank", replace = "air"},
+			{x = -1, z = -1, match = "nc_furniture:stick", replace = "air"},
+			{x = 1, z = -1, match = "nc_furniture:stick", replace = "air"},
+			{x = 2, z = -1, match = "nc_furniture:stick", replace = "air"},
+			{x = 3, z = -1, match = "nc_furniture:stick", replace = "air"},
 		},
 		items = {
-			modname .. ":chair_wood"
+			 "nc_furniture:chair_wood"
 		}
 	})
